ebb3c81a0a8c1def578b9f411527ebdd415ee1a3
Alan Marsh <marshy26@hotmail.com>
2015-10-08 11:53:26 +0100

    added a counter to User class and a hashmap for the id number in PacemakerAPI

--- src/models/User.java
+++ src/models/User.java
@@ -5,36 +5,41 @@ import com.google.common.base.Objects;
 
 public class User 
 {
-  public String firstName;
-  public String lastName;
-  public String email;
-  public String password;
-
-  public User()
-  {
-  }
-
-  public User(String firstName, String lastName, String email, String password)
-  {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.password = password;
-  }
-  
-  @Override  
-  public int hashCode()  
-  {  
-     return Objects.hashCode(this.lastName, this.firstName, this.email, this.password);  
-  }  
-  
-  @Override
-  public String toString()
-  {
-    return toStringHelper(this).addValue(firstName)
-                               .addValue(lastName)
-                               .addValue(password)
-                               .addValue(email)                               
-                               .toString();
-  }
+	static Long   counter = 0l;
+	
+	public String firstName;
+	public String lastName;
+	public String email;
+	public String password;
+	public Long   id;
+		
+	public User()
+	{
+	}
+		
+	public User(String firstName, String lastName, String email, String password)
+	{
+		this.id        = counter++;
+		
+	    this.firstName = firstName;
+	    this.lastName = lastName;
+	    this.email = email;
+	    this.password = password;
+	  }
+	  
+	  @Override  
+	  public int hashCode()  
+	  {  
+	     return Objects.hashCode(this.lastName, this.firstName, this.email, this.password);  
+	  }  
+	  
+	  @Override
+	  public String toString()
+	  {
+	    return toStringHelper(this).addValue(firstName)
+	                               .addValue(lastName)
+	                               .addValue(password)
+	                               .addValue(email)                               
+	                               .toString();
+	  }
 }
